3246c602804b3e89ba40a50f615f70624435325f
ChenYilong <luohanchenyilong@163.com>
2022-08-22 12:12:25 +1200

    feat: create SwiftUI Demo

---
+++ SwiftUI/PhotoAlbumGenerator/PhotoAlbumGenerator/PhotoAlbumGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoAlbumGeneratorApp.swift
+//  PhotoAlbumGenerator
+//
+//  Created by chenyilong on 2022/8/22.
+//
+
+import SwiftUI
+
+@main
+struct PhotoAlbumGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUI/PhotoAlbumGenerator/PhotoAlbumGeneratorUITests/PhotoAlbumGeneratorUITestsLaunchTests.swift
@@ -0,0 +1,31 @@
+//
+//  PhotoAlbumGeneratorUITestsLaunchTests.swift
+//  PhotoAlbumGeneratorUITests
+//
+//  Created by chenyilong on 2022/8/22.
+//
+
+import XCTest
+
+class PhotoAlbumGeneratorUITestsLaunchTests: XCTestCase {
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
