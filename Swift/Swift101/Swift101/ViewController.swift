fc1f40981aab72ba94c8e8876ac4344846e2b035
ChenYilong <luohanchenyilong@163.com>
2023-07-30 17:29:44 +1200

    docs: ✏️ Difference between map, flatMap and compactMap in Swift

--- Swift/Swift101/Swift101/ViewController.swift
+++ Swift/Swift101/Swift101/ViewController.swift
@@ -8,9 +8,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
+        testProtocolExtension()
+        arrayFilter()
+    }
+    
+    fileprivate func testProtocolExtension() {
         // Do any additional setup after loading the view.
         let aType: ClassIdenfifiable.Type = BaseCell.self
         print("aType.reuseIdentifier is ", aType.reuseIdentifier)
@@ -23,5 +27,20 @@ class ViewController: UIViewController {
         print("BaseCell.reuseIdentifier2 is ", BaseCell.reuseIdentifier2)
     }
     
+    func arrayFilter() {
+        let nestedArray = [[1, 2, 3], [4, 5, 6]]
+        let flattenedArray = nestedArray.flatMap { $0 }
+        print("flatMap ", flattenedArray) // Output: [1, 2, 3, 4, 5, 6]
+
+        
+        let stringNumbers = ["1", "2", "three", "4"]
+        let mappedNumbers = stringNumbers.compactMap { Int($0) }
+        print("compactMap ", mappedNumbers) // Output: [1, 2, 4]
+
+        let sampleNumbers = [1, 2, 3, 4]
+        let squaredNumbers = sampleNumbers.map { $0 * $0 }
+        print("compactMap ", squaredNumbers) // Output: [1, 4, 9, 16]
+
+    }
 }
 
--- Swift/Swift101/Swift101Tests/Swift101Tests.swift
+++ Swift/Swift101/Swift101Tests/Swift101Tests.swift
@@ -32,5 +32,32 @@ final class Swift101Tests: XCTestCase {
             // Put the code you want to measure the time of here.
         }
     }
+    fileprivate func testProtocolExtension() {
+        // Do any additional setup after loading the view.
+        let aType: ClassIdenfifiable.Type = BaseCell.self
+        print("aType.reuseIdentifier is ", aType.reuseIdentifier)
+        print("aType.reuseIdentifier2 is ", aType.reuseIdentifier2)
+        
+        //       ClassIdenfifiable is Existential type
+        //
+        let cell: ClassIdenfifiable = BaseCell()
+        print("BaseCell.reuseIdentifier is ", BaseCell.reuseIdentifier)
+        print("BaseCell.reuseIdentifier2 is ", BaseCell.reuseIdentifier2)
+    }
+    
+    func testArrayFilter() {
+        let nestedArray = [[1, 2, 3], [4, 5, 6]]
+        let flattenedArray = nestedArray.flatMap { $0 }
+        print("flatMap ", flattenedArray) // Output: [1, 2, 3, 4, 5, 6]
 
+        
+        let stringNumbers = ["1", "2", "three", "4"]
+        let mappedNumbers = stringNumbers.compactMap { Int($0) }
+        print("compactMap ", mappedNumbers) // Output: [1, 2, 4]
+
+        let sampleNumbers = [1, 2, 3, 4]
+        let squaredNumbers = sampleNumbers.map { $0 * $0 }
+        print("compactMap ", squaredNumbers) // Output: [1, 4, 9, 16]
+
+    }
 }
